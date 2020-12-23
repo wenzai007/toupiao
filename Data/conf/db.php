89db88f515ae06afd0f4a33b2d791ca5b5b0dec3
Wenze Zhang <wenzzha@microsoft.com>
2020-12-24 02:06:07 +0800

    make it work

--- Data/conf/db.php
+++ Data/conf/db.php
@@ -1,11 +1,11 @@
 <?php
 return array (
   'DB_TYPE' => 'mysql',
-  'DB_HOST' => '192.168.90.246',
+  'DB_HOST' => 'localhost',
   'DB_PORT' => '3306',
-  'DB_NAME' => 'toupiao',   //数据库名
+  'DB_NAME' => 'testforvote',   //数据库名
   'DB_USER' => 'root',   //用户名
-  'DB_PWD' => '123456',   //密码
+  'DB_PWD' => '13115365996',   //密码
   'DB_PREFIX' => 'tp_',
   'agent_version' => 1,
 );
